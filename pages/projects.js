75d7701699befc0d3cfd48922529af24d0d0ae32
Adithya <adithyapantangi123@gmail.com>
2022-09-02 23:00:14 +0530

    Updated Projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,3 +1,29 @@
 export default function projects() {
-  return <div>Projects</div>;
+  return (
+    <div
+      style={{
+        minWidth: "100vw",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <span style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+        Adithya Pantangi
+      </span>
+
+      <div
+        style={{
+          marginBottom: "300px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <span style={{ fontWeight: 600, fontSize: "50px" }}>Projects </span>
+        <span style={{ fontWeight: 600 }}>Coming Soon!</span>
+      </div>
+    </div>
+  );
 }
