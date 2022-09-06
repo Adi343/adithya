d6f9e74d68d32df317db80da4ee17d5bce118245
Adithya <adithyapantangi123@gmail.com>
2022-09-06 19:11:26 +0530

    Updated projects page

--- pages/projects.js
+++ pages/projects.js
@@ -16,14 +16,70 @@ export default function projects() {
 
       <div
         style={{
-          marginBottom: "300px",
+          // marginBottom: "30%",
           display: "flex",
           flexDirection: "column",
         }}
       >
-        <span style={{ fontWeight: 600, fontSize: "50px" }}>Projects </span>
-        <span style={{ fontWeight: 600 }}>Coming Soon!</span>
+        {/* <span style={{ fontWeight: 600, fontSize: "50px" }}>Projects </span>
+        <span style={{ fontWeight: 600 }}>Coming Soon!</span> */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ProjectCard />
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            {[1, 2, 3, 4, 5].map((item) => (
+              <CustomButton item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div>
+        <span>Made with NextJS</span>
       </div>
     </div>
   );
 }
+
+const ProjectCard = () => {
+  return (
+    <div
+      style={{
+        height: "110px",
+        width: "750px",
+        borderRadius: "10px",
+        border: "2px solid black",
+      }}
+    >
+      <span>Hello World</span>
+    </div>
+  );
+};
+
+const CustomButton = (props) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "30px",
+        height: "30px",
+        border: "3px solid black",
+        borderRadius: "20px",
+        margin: "3px",
+      }}
+    >
+      <span style={{}}>{props.item}</span>
+    </div>
+  );
+};
